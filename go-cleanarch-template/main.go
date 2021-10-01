44b43d59c3b3e4a4577f7b585657e9a250230ba7
Dall06 <devleon.06@gmail.com>
2021-10-01 01:31:07 -0500

    add: api v2 init

---
+++ go-cleanarch-template/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Dall06/go-cleanarch-template/pkg/cmd"
+)
+
+func main() {
+	// INIT APP
+	fmt.Println("Hello, Human!\nThis is the pssword-api-rest service")
+	cmd.RunApp()
+}
